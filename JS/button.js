86a08dcb764e4f2db24495d58687c78838e36a08
BrazilianBug <leandro.smacedo@Live.com>
2021-07-12 12:07:51 -0300

    Remoção de JavaScript

--- JS/button.js
+++
@@ -1,3 +0,0 @@
-const approve = "Cliente reconhece a compra.\nAprovado.";
-const fraud = "Cliente não reconhece a compra.\nConfirma nome completo e data de nascimento.\n\nFraude confirmada";
-const fraud2 = "Cliente reconhece a compra.\nConfirma nome completo mas se recusa a confirmar data de nascimento.\n\nFraude confirmada.";
