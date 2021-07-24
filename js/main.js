6274fc2f9b2e13080340c8c75a8398bf106b5d47
juanudk <juanudkp@gmail.com>
2021-07-24 19:04:27 -0300

    Solved "Universal time-based productivity marker"

--- js/main.js
+++ js/main.js
@@ -1,49 +1,61 @@
-var contadores = ["prod13h", "prod14h", "prod15h", "prod16h", "prod17h", "prod18h", "prod19h", "prod20h",
-"prod21h"
+var counters = ["prod13h", "prod14h", "prod15h", "prod16h", "prod17h", "prod18h", "prod19h", "prod20h",
+    "prod21h"
 ]
-displayHour();
-function displayHour() {    
-    d = new Date();
-    document.getElementById("timeText").innerText = d.getHours()+"h00";
+onLoad();
+function onLoad() {
+    counters.forEach((element) => {
+        if (!window.localStorage.getItem(element)) {
+            window.localStorage.setItem(element, 0)
+        }
+    })
+    prodId = getHourTemplate();
+    document.getElementById("localProd").innerHTML = window.localStorage.getItem(prodId);
+    displayHour();
+    calculate();
+    setInterval(displayHour, 60000);
 }
-setInterval(displayHour, 60000);
-
-calculate()
-contadores.forEach((element) => {
-if (!window.localStorage.getItem(element)) {
-    window.localStorage.setItem(element, 0)
+function displayHour() {
+    date = new Date();
+    prodId = getHourTemplate();
+    calculate();
+    document.getElementById("localProd").innerHTML = window.localStorage.getItem(prodId);
+    document.getElementById("timeText").innerText = date.getHours() + "h00";
 }
-document.getElementById(element).innerHTML = window.localStorage.getItem(element);
-})
-
-function copyText(text){
-navigator.clipboard.writeText(text); 
+function copyText(text) {
+    navigator.clipboard.writeText(text);
 }
-function calculate(){
-count=0
-contadores.forEach((element) => {
-    count+=parseInt(window.localStorage.getItem(element))
-})
-document.getElementById("txt_total").innerHTML = count;
+function calculate() {
+    count = 0;
+    counters.forEach((element) => {
+        count += parseInt(window.localStorage.getItem(element));
+    })
+    document.getElementById("txt_total").innerHTML = count;
 }
-function reset(){
-contadores.forEach((element) => {
-window.localStorage.setItem(element, 0)
-document.getElementById(element).innerHTML = window.localStorage.getItem(element)
-})
-calculate();
+function reset() {
+    counters.forEach((element) => {
+        window.localStorage.setItem(element, 0)
+        document.getElementById(element).innerHTML = window.localStorage.getItem(element)
+    })
+    calculate();
 }
-
-function add(id) {
-window.localStorage.setItem(id, parseInt(window.localStorage.getItem(id)) + 1)
-document.getElementById(id).innerHTML = window.localStorage.getItem(id)
-calculate();
+function add() {
+    prodId = getHourTemplate();
+    document.getElementById("timeText")
+    window.localStorage.setItem(prodId, parseInt(window.localStorage.getItem(prodId)) + 1)
+    document.getElementById("localProd").innerHTML = window.localStorage.getItem(prodId)
+    calculate();
 }
-
-function remove(id) {
-if(parseInt(window.localStorage.getItem(id)) > 0){
-    window.localStorage.setItem(id, parseInt(window.localStorage.getItem(id)) - 1)
-    document.getElementById(id).innerHTML = window.localStorage.getItem(id)
+function remove() {
+    prodId = getHourTemplate();
+    if (parseInt(window.localStorage.getItem(prodId)) > 0) {
+        window.localStorage.setItem(prodId, parseInt(window.localStorage.getItem(prodId)) - 1)
+        document.getElementById("localProd").innerHTML = window.localStorage.getItem(prodId)
+    }
+    calculate();
 }
-calculate();
+function getHourTemplate() {
+    date = new Date();
+    hour = date.getHours()
+    prodId = `prod${hour}h`
+    return prodId
 }
