3126c4ea4352a1a96fbfaf6888f55764a112e3a4
Leandro <leandro.smacedo@live.com>
2021-07-28 14:21:35 -0300

    Quality of life update.
    The "change performed" notice has been changed to text within the History DIV.
    
    The Reset Button has been moved to the bottom of the page.
    
    Grammatical changes were made in the texts that were copied to the user in the tags' buttons.

--- js/button.js
+++ js/button.js
@@ -1,9 +1,9 @@
 const ovp = "#OVP."
 const exp ="#VIPEXPRESSO."
-const approve = "Cliente reconhece a compra.\nAprovado.";
-const website = "Cliente reconhece a compra.\nInforma que efetuou a compra pelo site.\n\nAprovado."
-const app = "Cliente reconhece a compra.\nInforma que efetuou a compra pelo App.\n\nAprovado."
-const outros ="Cliente reconhece a compra.\nInforma que efetuou a compra ultilizado o canal de vendas ***\nconfirma data de nascimento e últimos 3 dígitos do cartão.\n\nAprovado."
-const fraude = "Cliente não reconhece a compra.\nInforma que não autorizou terceiros a efetuarem compras em seu nome.\nConfirma nome completo e data de nascimento.\n\nFraude confirmada";
-const fraude2 = "Cliente reconhece a compra.\nInforma que não autorizou terceiros a efetuarem compras em seu nome.\nConfirma nome completo mas se recusa a confirmar data de nascimento.\n\nFraude confirmada.";
-const cancel = "Cliente reconhece a compra mas solicita o cancelamento\nConfirma nome completo e data de nascimento\n\nCancelado."
+const approve = "Cliente confirmou ter realizado a compra.\nAprovado.";
+const website = "Cliente confirmou ter realizado a compra pelo site.\n\nAprovado."
+const app = "Cliente confirmou ter realizado a compra pelo Aplicativo.\n\nAprovado."
+const outros ="Cliente confirmou ter realizado a compra por meio do canal de vendas intermediario ***.\nCliente informou corretamente os três últimos digitos do cartão e e-mail.\n\nAprovado."
+const fraude = "Cliente não reconhece a compra.\nInforma não ter autorizado terceiros a efetuarem compras em seu nome.\nConfirma nome completo e data de nascimento.\n\nFraude confirmada.";
+const fraude2 = "Cliente não reconhece a compra.\nInforma não ter autorizado terceiros a efetuarem compras em seu nome.\nConfirma nome completo mas se recusa a confirmar data de nascimento.\n\nFraude confirmada.";
+const cancel = "Titular do pedido não deseja prosseguir com a compra.\nConfirma nome completo e data de nascimento\n\nCancelado."
